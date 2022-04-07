acad7e15fe7ba0849360d50cb4f8b7f4ca1b7b27
Codeno-ob <97269929+Codeno-ob@users.noreply.github.com>
2022-04-07 21:07:48 +0530

    Update hourglass pattern.py

--- hourglass pattern.py
+++ hourglass pattern.py
@@ -1,12 +1,20 @@
-for i in range(5):
-    for j in range(i):
-        print(' ',end='')
-    for k in range(5-i):
-        print('*',end=' ')
-    print('\n')
-for i in range(4,0,-1):
-    for j in range(1,i):
-        print(' ',end='')
-    for k in range(i,6):
-        print('*',end=' ')
-    print('\n')
+for i in range(5,1,-1):
+    
+    print(" "*(5-i) + "* " * i)
+    
+    
+for i in range(1,6):
+    print(" "*(5-i) + "* " * i)
+    
+print('\n')
+i = 5
+while i>1:
+ 
+   print(" "*(5-i) + "* " * i)
+   i-=1
+
+i = 1
+while i<=5:
+   
+   print(" "*(5-i) + "* " * i)
+   i+=1
